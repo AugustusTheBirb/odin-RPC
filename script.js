0fdda57104ce987bfc50118762488157aaa6fe4f
AugustusTheBirb <augustasrink@gmail.com>
2025-02-01 01:10:37 +0200

    added play round function and result printing

--- script.js
+++ script.js
@@ -7,17 +7,35 @@ let rewardMatrix = [
     [[0,1],[1,0],[0,0]],
 ];
 
-let dict = {
-    rock: 0,
-    paper: 1,
-    scissors: 2
-};
+let names = ["rock", "paper", "scissors"];
+
+function capitalize(s){
+    return s[0].toUpperCase() + s.slice(1);
+}
 
 function getPlayerChoice(){
     choice = prompt("Rock paper or scissors?").toLowerCase();
-    return dict[choice];
+    return names.indexOf(choice);
 }
 
 function getComputerChoice(){
     return Math.floor(Math.random() * 3);
 }
+
+function playRound(playerChoice, computerChoice){
+    score = rewardMatrix[playerChoice][computerChoice];
+    playerScore += score[0];
+    computerScore += score[1];
+    if (score[0] === 0 && score[1] === 0) {
+        console.log("It's a tie!");
+    }
+    else if (score[0] === 1) {
+        console.log(`You win, ${capitalize(names[playerChoice])} beats ${capitalize(names[computerChoice])}`);
+    }
+    else {
+        console.log(`You lose, ${capitalize(names[computerChoice])} beats ${capitalize(names[playerChoice])}`);
+    }
+    console.log(`The scores are: You-${playerScore}, Computer-${computerScore}`)
+}
+
+playRound(0,0)
