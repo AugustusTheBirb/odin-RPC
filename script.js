24ec45a6f7f7bca8f100a7df74447d4ed3393f82
AugustusTheBirb <augustasrink@gmail.com>
2025-02-01 00:50:16 +0200

    added choice functions

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log("test")
+let computerScore = 0;
+let playerScore = 0;
+
+let rewardMatrix = [
+    [[0,0],[0,1],[1,0]],
+    [[1,0],[0,0],[0,1]],
+    [[0,1],[1,0],[0,0]],
+];
+
+let dict = {
+    rock: 0,
+    paper: 1,
+    scissors: 2
+};
+
+function getPlayerChoice(){
+    choice = prompt("Rock paper or scissors?").toLowerCase();
+    return dict[choice];
+}
+
+function getComputerChoice(){
+    return Math.floor(Math.random() * 3);
+}
